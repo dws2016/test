443dfe650ebc8c2ed594291d8e8b4e8e7b19622a
dws <dwsandroid@163.com>
2016-03-19 10:54:34 +0800

    init 3.0

--- src/cn/itcast/animation/MainActivity.java
+++ src/cn/itcast/animation/MainActivity.java
@@ -3,13 +3,20 @@ package cn.itcast.animation;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private Button btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btn = (Button) findViewById(R.id.btn);
+		btn.setText("without hope");
+		
 	}
 
 }
