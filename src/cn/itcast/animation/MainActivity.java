a2a48aa27a79a47feb60aba4fdab5e77a656f311
dws <dwsandroid@163.com>
2016-03-19 11:19:27 +0800

    init 4.0

--- src/cn/itcast/animation/MainActivity.java
+++ src/cn/itcast/animation/MainActivity.java
@@ -1,7 +1,10 @@
 package cn.itcast.animation;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +13,14 @@ public class MainActivity extends Activity {
 
 	private Button btn;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		btn = (Button) findViewById(R.id.btn);
 		btn.setText("without hope");
+		btn.setBackground(new ColorDrawable(Color.RED));
 		
 	}
 
